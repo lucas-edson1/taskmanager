7f34d1f19016cb445ade04109c1f1561e090f67b
Lucas <lucasedson03@gmail.com>
2024-07-12 21:11:53 -0300

    Back-end com a função de cadastrar tarefas

--- src/main/java/io/github/lucasedson/taskmanager/control/TaskControl.java
+++ src/main/java/io/github/lucasedson/taskmanager/control/TaskControl.java
@@ -1,13 +1,39 @@
 package io.github.lucasedson.taskmanager.control;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.github.lucasedson.taskmanager.model.TaskModel;
+import io.github.lucasedson.taskmanager.repository.TaskRepository;
+import io.github.lucasedson.taskmanager.service.TaskService;
 
 @RestController
 public class TaskControl {
     
+    @Autowired 
+    private TaskService servico;
+
+    @Autowired
+    private TaskRepository repository;
+
     @GetMapping("/")
     public String mainPage(){
         return "Rota Inicial";
+    }
+
+    @GetMapping("/listar")
+    public List<TaskModel> listar(){
+        return repository.findAll();
+    }
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<TaskModel> cadastrar(@RequestBody TaskModel obj){
+        return servico.cadastrar(obj);
     }
 }
--- src/main/java/io/github/lucasedson/taskmanager/service/TaskService.java
+++ src/main/java/io/github/lucasedson/taskmanager/service/TaskService.java
@@ -1,5 +1,23 @@
 package io.github.lucasedson.taskmanager.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import io.github.lucasedson.taskmanager.model.Mensagem;
+import io.github.lucasedson.taskmanager.model.TaskModel;
+import io.github.lucasedson.taskmanager.repository.TaskRepository;
+
+@Service
 public class TaskService {
-    
+    @Autowired 
+    private Mensagem msg;
+
+    @Autowired 
+    private TaskRepository rep;
+
+    public ResponseEntity<TaskModel> cadastrar(TaskModel obj){
+        return new ResponseEntity<>(rep.save(obj), HttpStatus.CREATED);
+    }
 }
